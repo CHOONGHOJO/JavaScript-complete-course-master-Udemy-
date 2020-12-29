031f25c8115bcbd8ccd44fd722eb924f1c9c2a70
CHOONGHOJO <cjoe731@hotmail.com>
2020-12-29 09:45:30 -0500

    searchView edited

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -1,16 +1,19 @@
 class SearchView {
   _parentEl = document.querySelector('.search');
 
+
   getQuery() {
       const query =  this._parentEl.querySelector('.search__field').value;
       this._clearInput();
       return query;
   }
 
+
   _clearInput() {
     this._parentEl.querySelector('.search__field').value = '';
     
   }
+
 
   addHandlerSearch(handler) {
       this._parentEl.addEventListener('submit', function(e) {
@@ -19,5 +22,6 @@ class SearchView {
       });
   }
 }
+
 
 export default new SearchView();
